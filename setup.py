9173a9141031aad25b1e3ea9d68f5a534b0177b1
Richard Kiss <him@richardkiss.com>
2016-07-26 15:10:29 -0700

    Get tests working.

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
         "pycoin.ecdsa.native",
         "pycoin.key",
         "pycoin.message",
+        "pycoin.networks",
         "pycoin.tx",
         "pycoin.tx.pay_to",
         "pycoin.tx.script",
